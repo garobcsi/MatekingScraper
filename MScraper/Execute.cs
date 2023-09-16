2ccb91208013c1c5b53f5969b2b3dcbf9feba78e
garobcsi <gasparrobert0908@gmail.com>
2023-09-16 18:57:25 +0200

    Changed exec command

--- MScraper/Execute.cs
+++ MScraper/Execute.cs
@@ -3,7 +3,7 @@ using System.Diagnostics;
 
 public class Execute
 {
-    public static void exec(string command)
+    public static async Task exec(string command)
     {
         ProcessStartInfo procStartInfo = new ProcessStartInfo("/bin/bash",$"-c \"{command}\"");
         Process proc = new Process() { StartInfo = procStartInfo, };
