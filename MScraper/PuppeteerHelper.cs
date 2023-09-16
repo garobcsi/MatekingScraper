64a1c0207c1203534e152f471275821f868ec111
garobcsi <gasparrobert0908@gmail.com>
2023-09-16 18:57:09 +0200

    Implamented Login

--- MScraper/PuppeteerHelper.cs
+++ MScraper/PuppeteerHelper.cs
@@ -7,6 +7,14 @@ public class PuppeteerHelper
     {
         DotEnv.Load();
         var envVars = DotEnv.Read();
-
+        
+        await PuppeteerInstance.page.WaitForSelectorAsync("#mathsplain-login-pane-opener");
+        var user = await PuppeteerInstance.page.QuerySelectorAsync("#mathsplain-login-pane-opener");
+        await user.ClickAsync();
+        
+        await PuppeteerInstance.page.TypeAsync("#edit-name",envVars["name"]);
+        await PuppeteerInstance.page.TypeAsync("#edit-pass", envVars["password"]);
+        var login = await PuppeteerInstance.page.QuerySelectorAsync("#edit-submit");
+        await login.ClickAsync();
     }
 }
