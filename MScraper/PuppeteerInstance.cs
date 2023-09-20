7b12b17c0bdfbcceb9ed07b4b4d8c673878f19a0
garobcsi <gasparrobert0908@gmail.com>
2023-09-20 15:05:25 +0200

    Updated window height and with made to use Headles

--- MScraper/PuppeteerInstance.cs
+++ MScraper/PuppeteerInstance.cs
@@ -6,8 +6,8 @@ public class PuppeteerInstance {
     private bool Headless = false;
     // private int Width = 1920;
     // private int Height = 1080;
-    private int Width = 900;
-    private int Height = 900;
+    private int Width = 600;
+    private int Height = 600;
     
     public static IBrowser browser { get; private set; }
     public static IPage page{ get; private set; }
@@ -17,7 +17,7 @@ public class PuppeteerInstance {
         BrowserFetcher browserFetcher = new BrowserFetcher();
         await browserFetcher.DownloadAsync();
         browser = await Puppeteer.LaunchAsync(
-            new LaunchOptions { Headless = false });
+            new LaunchOptions { Headless = Headless });
         page = await browser.NewPageAsync();
         await page.SetViewportAsync(new ViewPortOptions
         {
