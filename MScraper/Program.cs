a86b369134f95290340d5e08ef08e7cd3b47ebc9
garobcsi <gasparrobert0908@gmail.com>
2023-09-20 15:06:06 +0200

    Made checking for env files

--- MScraper/Program.cs
+++ MScraper/Program.cs
@@ -1,5 +1,10 @@
-﻿using MScraper;
+using MScraper;
 
+if (!File.Exists(".env"))
+{
+    ColorPrintHelper.WriteLine("No .env file located.",ConsoleColor.Red);
+    ExitHelper.Exit();
+}
 await new PuppeteerInstance().Init();
 await PuppeteerHelper.Login();
 
