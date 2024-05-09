2a931194637544407866e4a40b911d8df4e9c2c7
garobcsi <gasparrobert0908@gmail.com>
2024-05-09 17:43:19 +0200

    Refactored PupInst

--- src/Links.cs
+++ src/Links.cs
@@ -1,8 +1,8 @@
-namespace MScraper;
+namespace MathScraper;
 
 public class Links
 {
-    public static string Base = "h"+"t"+"t"+"p"+"s"+":"+"/"+"/"+"w"+"w"+"w"+"."+"m"+"a"+"t"+"e"+"k"+"i"+"n"+"g"+"."+"h"+"u"+"/";
+    public static string Base = "h" + "t" + "t" + "p" + "s" + ":" + "/" + "/" + "w" + "w" + "w" + "." + "m" + "a" + "t" + "e" + "k" + "i" + "n" + "g" + "." + "h" + "u" + "/";
 
     public static string MyCourses = "my-courses";
 }
--- src/PuppeteerInstance.cs
+++ src/PuppeteerInstance.cs
@@ -1,29 +1,34 @@
-namespace MScraper;
 using PuppeteerSharp;
 
-public class PuppeteerInstance {
-    
-    private bool Headless = false;
-    // private int Width = 1920;
-    // private int Height = 1080;
-    private int Width = 600;
-    private int Height = 600;
-    
-    public static IBrowser browser { get; private set; }
-    public static IPage page{ get; private set; }
-    
+namespace MathScraper;
+
+public class PuppeteerInstance
+{
+    private const bool Headless = false;
+
+    private const int Width = 600;
+    private const int Height = 600;
+
+    public static IBrowser? Browser { get; private set; } = null;
+    public static IPage? Page { get; private set; } = null;
+
     public async Task Init()
     {
-        BrowserFetcher browserFetcher = new BrowserFetcher();
+        var browserFetcher = new BrowserFetcher();
         await browserFetcher.DownloadAsync();
-        browser = await Puppeteer.LaunchAsync(
+        Browser = await Puppeteer.LaunchAsync(
             new LaunchOptions { Headless = Headless });
-        page = await browser.NewPageAsync();
-        await page.SetViewportAsync(new ViewPortOptions
+        Page = await Browser.NewPageAsync();
+        await Page.SetViewportAsync(new ViewPortOptions
         {
-            Width = this.Width,
-            Height = this.Height
+            Width = Width,
+            Height = Height
         });
-        await page.GoToAsync(Links.Base);
+        await Page.GoToAsync(Links.Base);
+    }
+    ~PuppeteerInstance()
+    {
+        Browser = null;
+        Page = null;
     }
 }
