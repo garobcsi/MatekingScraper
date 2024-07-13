e25e08eca82eb2231f64493fb49a7288ead01369
garobcsi <gasparrobert0908@gmail.com>
2024-07-13 18:35:09 +0200

    Made terminal color helper

---
+++ src/PrintColor.cs
@@ -0,0 +1,19 @@
+namespace MathScraper;
+
+public class PrintColor
+{
+    public static void WriteLine(string str, ConsoleColor? foreground = null, ConsoleColor? background = null)
+    {
+        if (foreground != null) Console.ForegroundColor = (ConsoleColor)foreground;
+        if (background != null) Console.BackgroundColor = (ConsoleColor)background;
+        Console.WriteLine(str);
+        Console.ResetColor();
+    }
+    public static void Write(string str, ConsoleColor? foreground = null, ConsoleColor? background = null)
+    {
+        if (foreground != null) Console.ForegroundColor = (ConsoleColor)foreground;
+        if (background != null) Console.BackgroundColor = (ConsoleColor)background;
+        Console.Write(str);
+        Console.ResetColor();
+    }
+}
