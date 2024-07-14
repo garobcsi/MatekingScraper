0f44630444087d7ad4f456e4c9bbecf3da585381
garobcsi <gasparrobert0908@gmail.com>
2024-07-14 15:36:11 +0200

    Updated Main To Fix Github Actions

--- src/Program.cs
+++ src/Program.cs
@@ -1,13 +1,3 @@
-using MScraper;
-using MScraper.Model;
+using MathScraper;
 
-if (!File.Exists(".env"))
-{
-    ColorPrintHelper.WriteLine("No .env file located.",ConsoleColor.Red);
-    ExitHelper.Exit();
-}
-await new PuppeteerInstance().Init();
-await PuppeteerHelper.Login();
-List<MyCourse> MyCourses = await PuppeteerHelper.GetMyCourses();
-
-await PuppeteerHelper.BrowserExit();
+Console.WriteLine("Test");
