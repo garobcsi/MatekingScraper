43225264a137f2389a19fea9dbe1a3a2f3d65cfd
garobcsi <gasparrobert0908@gmail.com>
2024-07-14 15:33:54 +0200

    Updated AsyncJobQueue to have max Jobs running

--- src/AsyncJobQueue.cs
+++ src/AsyncJobQueue.cs
@@ -4,13 +4,40 @@ namespace MathScraper;
 
 public class AsyncJobQueue
 {
-    private ConcurrentDictionary<Guid, (Func<Guid, CancellationToken, Task> job, Task task, CancellationTokenSource cts)> _jobs = new();
+    private readonly ConcurrentDictionary<Guid, (Func<Guid, CancellationToken, Task> job, Task task, CancellationTokenSource cts)> _jobs = new();
+    private SemaphoreSlim _semaphore = null!;
+    private bool _concurrencyLimitEnabled;
+    private int _maxConcurrentJobs;
+
+    public AsyncJobQueue(bool concurrencyLimitEnabled = false, int maxConcurrentJobs = 10)
+    {
+        _concurrencyLimitEnabled = concurrencyLimitEnabled;
+        _maxConcurrentJobs = maxConcurrentJobs;
+        if (_concurrencyLimitEnabled)
+        {
+            _semaphore = new SemaphoreSlim(_maxConcurrentJobs, _maxConcurrentJobs);
+        }
+    }
 
     public Guid AddJob(Func<Guid, CancellationToken, Task> job)
     {
         Guid jobId = Guid.NewGuid();
         CancellationTokenSource cts = new();
-        Task task = ProcessJob(jobId, job, cts.Token);
+        
+        var task = Task.Run(async () =>
+        {
+            if (_concurrencyLimitEnabled) await _semaphore.WaitAsync(cts.Token);
+
+            try
+            {
+                await ProcessJob(jobId, job, cts.Token);
+            }
+            finally
+            {
+                if (_concurrencyLimitEnabled) _semaphore.Release();
+            }
+        }, cts.Token);
+
         _jobs.TryAdd(jobId, (job, task, cts));
         return jobId;
     }
@@ -35,13 +62,11 @@ public class AsyncJobQueue
         }
     }
 
-    public void RemoveJob(Guid jobId)
+    private void RemoveJob(Guid jobId)
     {
-        if (_jobs.TryRemove(jobId, out var jobEntry))
-        {
-            jobEntry.cts.Cancel();
-            jobEntry.cts.Dispose();
-        }
+        if (!_jobs.TryRemove(jobId, out var jobEntry)) return;
+        jobEntry.cts.Cancel();
+        jobEntry.cts.Dispose();
     }
 
     public void CancelJob(Guid jobId)
@@ -63,5 +88,36 @@ public class AsyncJobQueue
     public async Task WaitForAllJobsAsync()
     {
         await Task.WhenAll(_jobs.Values.Select(j => j.task));
+    }
+
+    public int RunningJobsCount => _jobs.Count;
+
+    public bool ConcurrencyLimitEnabled
+    {
+        get => _concurrencyLimitEnabled;
+        set
+        {
+            _concurrencyLimitEnabled = value;
+            if (_concurrencyLimitEnabled)
+            {
+                _semaphore = new SemaphoreSlim(_maxConcurrentJobs, _maxConcurrentJobs);
+            }
+            else
+            {
+                _semaphore.Dispose();
+            }
+        }
+    }
+
+    public int MaxConcurrentJobs
+    {
+        get => _maxConcurrentJobs;
+        set
+        {
+            _maxConcurrentJobs = value;
+            if (!_concurrencyLimitEnabled) return;
+            _semaphore.Release();
+            _semaphore = new SemaphoreSlim(_maxConcurrentJobs, _maxConcurrentJobs);
+        }
     }
 }
