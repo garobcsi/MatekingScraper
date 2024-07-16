497eaf30bdc74aa28773d8611bdbf5afe8ac7af5
garobcsi <gasparrobert0908@gmail.com>
2024-07-16 20:03:49 +0200

    Added example code

--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,17 @@
 using MathScraper;
+using PuppeteerSharp;
 
-Console.WriteLine("Test");
+BrowserInstance bwi = await BrowserInstance.Init();
+
+AsyncJobQueue jobQueue = new(true,1);
+for (int i = 0; i < 1; i++)
+{
+    jobQueue.AddJob(async (id, token) =>
+    {
+        PageInstance pai = await PageInstance.Init(bwi);
+        await pai.Page.CloseAsync();
+    });
+}
+
+await jobQueue.WaitForAllJobsAsync();
+await bwi.Browser.CloseAsync();
