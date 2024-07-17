3ae55bdf94473469369e8fe8f1d95c43c67ae81c
garobcsi <gasparrobert0908@gmail.com>
2024-07-17 16:36:20 +0200

    Added new link

--- src/Links.cs
+++ src/Links.cs
@@ -5,4 +5,6 @@ public class Links
     public static string Base = "h" + "t" + "t" + "p" + "s" + ":" + "/" + "/" + "w" + "w" + "w" + "." + "m" + "a" + "t" + "e" + "k" + "i" + "n" + "g" + "." + "h" + "u" + "/";
 
     public static string MyCourses = "my-courses";
+    public static string AllUniversitySubjects = "osszes-egyetemi-tantargy";
+    
 }
