6e454957447b0197c980ac62f7ffe332d5f79420
garobcsi <gasparrobert0908@gmail.com>
2024-07-21 17:01:35 +0200

    added .env to zips

--- publish.sh
+++ publish.sh
@@ -1,13 +1,13 @@
 dotnet publish -c Release -r linux-x64 --self-contained true -p:PublishSingleFile=true -p:PublishReadyToRun=true -p:PublishTrimmed=false
 dotnet publish -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true -p:PublishReadyToRun=true -p:PublishTrimmed=false
 
-cp ./src/.env.example ./src/bin/Release/net8.0/win-x64/publish/.env.example
-cp ./src/.env.example ./src/bin/Release/net8.0/linux-x64/publish/.env.example
+cp ./src/.env.example ./src/bin/Release/net8.0/win-x64/publish/.env
+cp ./src/.env.example ./src/bin/Release/net8.0/linux-x64/publish/.env
 
 cd ./src/bin/Release/net8.0/win-x64/publish
-zip -r ../../../../../../MathScraper-windows.zip ./*
+zip -r ../../../../../../MathScraper-windows.zip ./* .env
 
 cd ../../../../../..
 
 cd ./src/bin/Release/net8.0/linux-x64/publish
-tar -zcf ../../../../../../MathScraper-linux.tar.gz *
+tar -zcf ../../../../../../MathScraper-linux.tar.gz * .env
