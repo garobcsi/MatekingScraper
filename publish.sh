3889835a395739f6feb4df77e134b793ba916d10
garobcsi <gasparrobert0908@gmail.com>
2024-07-21 16:40:57 +0200

    added more publish actions

--- publish.sh
+++ publish.sh
@@ -1,2 +1,8 @@
 dotnet publish -c Release -r linux-x64 --self-contained true -p:PublishSingleFile=true -p:PublishReadyToRun=true -p:PublishTrimmed=false
-dotnet publish -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true -p:PublishReadyToRun=true -p:PublishTrimmed=false 
+dotnet publish -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true -p:PublishReadyToRun=true -p:PublishTrimmed=false
+
+cp ./src/.env.example ./src/bin/Release/net8.0/win-x64/publish/.env
+cp ./src/.env.example ./src/bin/Release/net8.0/linux-x64/publish/.env
+
+zip -r MathScraper-windows.zip ./src/bin/Release/net8.0/win-x64/publish
+tar -zcf MathScraper-linux.tar.gz ./src/bin/Release/net8.0/linux-x64/publish
