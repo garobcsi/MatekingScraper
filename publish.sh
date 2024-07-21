477fdc88d7680839dd95d8ca4421152a0bb79f9b
garobcsi <gasparrobert0908@gmail.com>
2024-07-21 20:18:45 +0200

    added ffmpeg for windows version

--- publish.sh
+++ publish.sh
@@ -2,6 +2,9 @@ dotnet publish -c Release -r linux-x64 --self-contained true -p:PublishSingleFil
 dotnet publish -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true -p:PublishReadyToRun=true -p:PublishTrimmed=false
 
 cp ./src/.env.example ./src/bin/Release/net8.0/win-x64/publish/.env
+wget https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl.zip -O ffmpeg.zip
+unzip -p myarchive.zip ffmpeg-master-latest-win64-gpl/bin/ffmpeg.exe > ./src/.env.example ./src/bin/Release/net8.0/win-x64/publish/ffmpeg.exe
+
 cp ./src/.env.example ./src/bin/Release/net8.0/linux-x64/publish/.env
 
 cd ./src/bin/Release/net8.0/win-x64/publish
