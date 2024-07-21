7e8b6724864fc57c64fc0fb18c6e6f1f4f1e70aa
garobcsi <gasparrobert0908@gmail.com>
2024-07-21 20:24:13 +0200

    Removed typos

--- publish.sh
+++ publish.sh
@@ -3,7 +3,7 @@ dotnet publish -c Release -r win-x64 --self-contained true -p:PublishSingleFile=
 
 cp ./src/.env.example ./src/bin/Release/net8.0/win-x64/publish/.env
 wget https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl.zip -O ffmpeg.zip
-unzip -p myarchive.zip ffmpeg-master-latest-win64-gpl/bin/ffmpeg.exe > ./src/.env.example ./src/bin/Release/net8.0/win-x64/publish/ffmpeg.exe
+unzip -p ffmpeg.zip ffmpeg-master-latest-win64-gpl/bin/ffmpeg.exe > ./src/bin/Release/net8.0/win-x64/publish/ffmpeg.exe
 
 cp ./src/.env.example ./src/bin/Release/net8.0/linux-x64/publish/.env
 
