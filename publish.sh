8cf0a7eb23e63b4f6fb11e4d133c5c457f044483
garobcsi <gasparrobert0908@gmail.com>
2024-07-21 16:52:21 +0200

    modified publish

--- publish.sh
+++ publish.sh
@@ -1,8 +1,11 @@
 dotnet publish -c Release -r linux-x64 --self-contained true -p:PublishSingleFile=true -p:PublishReadyToRun=true -p:PublishTrimmed=false
 dotnet publish -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true -p:PublishReadyToRun=true -p:PublishTrimmed=false
 
-cp ./src/.env.example ./src/bin/Release/net8.0/win-x64/publish/.env
-cp ./src/.env.example ./src/bin/Release/net8.0/linux-x64/publish/.env
+cp ./src/.env.example ./src/bin/Release/net8.0/win-x64/publish/.env.example
+cp ./src/.env.example ./src/bin/Release/net8.0/linux-x64/publish/.env.example
 
-zip -r MathScraper-windows.zip ./src/bin/Release/net8.0/win-x64/publish
-tar -zcf MathScraper-linux.tar.gz ./src/bin/Release/net8.0/linux-x64/publish
+cd ./src/bin/Release/net8.0/win-x64/publish
+zip -r ../../../../../../MathScraper-windows.zip ./*
+
+cd ./src/bin/Release/net8.0/linux-x64/publish
+tar -zcf ../../../../../../MathScraper-linux.tar.gz *
