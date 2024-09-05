84e61826164162995afd22be51e0ecefb27febcf
garobcsi <gasparrobert0908@gmail.com>
2024-09-05 13:04:19 +0200

    Added more file and folder fix

--- src/PageInstance.cs
+++ src/PageInstance.cs
@@ -61,7 +61,7 @@ public class PageInstance
             (char)11, (char)12, (char)13, (char)14, (char)15, (char)16, (char)17, (char)18, (char)19, (char)20,
             (char)21, (char)22, (char)23, (char)24, (char)25, (char)26, (char)27, (char)28, (char)29, (char)30,
             (char)31
-        }) + "?";
+        }) + "?" + new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
         Regex r = new Regex(string.Format("[{0}]", Regex.Escape(regexSearch)));
         return r.Replace(str ?? "", "");
     }
